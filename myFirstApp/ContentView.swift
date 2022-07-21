2d58a930f13142d032598f4fef263c9dffd86315
joseDev <jose96corrza@gmail.com>
2022-07-21 18:42:06 -0500

    Initial Commit

---
+++ myFirstApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  myFirstApp
+//
+//  Created by Jose Daniel Corredor Zambrano on 21/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ myFirstApp/myFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myFirstAppApp.swift
+//  myFirstApp
+//
+//  Created by Jose Daniel Corredor Zambrano on 21/07/22.
+//
+
+import SwiftUI
+
+@main
+struct myFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
